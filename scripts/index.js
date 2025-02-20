f6a5393f65455bbc99b785bf3be029daba9ef867
Daniil Kalmykov <kwerdk@yandex.ru>
2025-02-20 18:18:13 +0300

    secont iteration

--- scripts/index.js
+++ scripts/index.js
@@ -5,32 +5,32 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCards(cardData, cardDeleate) {
+function createCards(cardData, cardDelete) {
     // клонируем шаблон
-    const cardElement = cardTemplate.cloneNode(true);
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
     // устанавливаем содержимое
     const cardImage = cardElement.querySelector('.card__image');
     const cardTitle = cardElement.querySelector('.card__title');
-    const deleateButton = cardElement.querySelector('.card__delete-button');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
     cardImage.src = cardData.link;
     cardTitle.textContent = cardData.name;
 
     //вешаем клик
-    deleateButton.addEventListener('click', () => {
-        cardDeleate(cardElement);
+    deleteButton.addEventListener('click', () => {
+        cardDelete(cardElement);
     });
 
     return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function deleateCard(cardElement) {
+function deleteCard(cardElement) {
     cardElement.remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardData) => {
-    const card = createCard(cardData, deleateCard);
+    const card = createCards(cardData, deleteCard);
     placesList.append(card);
 })
